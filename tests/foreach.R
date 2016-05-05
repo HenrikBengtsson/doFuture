b6f330a93d2cc2182df73d68564c036a297d9183
hb <hb@aroma-project.org>
2016-05-04 22:43:54 -0700

    Add first package test

---
+++ tests/foreach.R
@@ -0,0 +1,32 @@
+library("doFuture")
+registerDoFuture()
+
+strategies <- future:::supportedStrategies()
+strategies <- setdiff(strategies, "multiprocess")
+
+message("*** doFuture() ...")
+
+res0 <- NULL
+
+for (strategy in strategies) {
+  message(sprintf("- plan('%s') ...", strategy))
+  plan(strategy)
+
+  mu <- 1.0
+  sigma <- 2.0
+  res <- foreach(i=1:3) %dopar% {
+    set.seed(0xBEEF)
+    rnorm(i, mean=mu, sd=sigma)
+  }
+  print(res)
+
+  if (is.null(res0)) {
+    res0 <- res
+  } else {
+    stopifnot(all.equal(res, res0))
+  }
+
+  message(sprintf("- plan('%s') ... DONE", strategy))
+} ## for (strategy ...)
+
+message("*** doFuture() ... DONE")
