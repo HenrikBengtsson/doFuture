b2b1ca6f9655da95355f204c99d74dbe5ce0f893
hb <hb@aroma-project.org>
2016-06-24 20:15:16 -0700

    TESTS: Ah... doRNG.dorng.R test may fail because of https://github.com/renozao/doRNG/issues/1

--- tests/doRNG,dorng.R
+++ tests/doRNG,dorng.R
@@ -9,13 +9,6 @@ if (require("doRNG")) {
   message("*** doFuture() w/ doRNG + %dorng% ...")
   print(sessionInfo())
 
-  ## There's a bug in doRNG (<= 1.6.0) causing the first iteration
-  ## of these tests to fail due to non-reproducibility of s1 and s1.2,
-  ## cf. https://github.com/renozao/doRNG/issues/1.  /HB 2016-05-07
-  if (packageVersion("doRNG") <= "1.6.0") {
-    strategies <- setdiff(strategies, c("eager", "lazy"))
-  }
-
   for (strategy in strategies) {
     message(sprintf("- plan('%s') ...", strategy))
     plan(strategy)
@@ -41,8 +34,13 @@ if (require("doRNG")) {
     s1.2 <- foreach(i=1:4) %dorng% { runif(1) }
     s2.2 <- foreach(i=1:4) %dorng% { runif(1) }
     str(list(s1.2=s1.2, s2.2=s2.2))
-    stopifnot(all.equal(s1.2, s1), all.equal(s2.2, s2))
-    stopifnot(identical(s1, s1.2), identical(s2, s2.2))
+
+    ## There's a bug in doRNG (<= 1.6.0) causing the first iteration
+    ## of these tests to fail due to non-reproducibility of s1 and s1.2,
+    ## cf. https://github.com/renozao/doRNG/issues/1.  /HB 2016-05-07
+    if (packageVersion("doRNG") > "1.6.0") {
+      stopifnot(identical(s1, s1.2), identical(s2, s2.2))
+    }
 
     message(sprintf("- plan('%s') ... DONE", strategy))
   } ## for (strategy ...)
