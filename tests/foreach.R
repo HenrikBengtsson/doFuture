370d4eaf0ca02ca33656974e10c4a17f35402fc4
hb <hb@aroma-project.org>
2016-06-24 18:11:00 -0700

    TESTS: Added package tests validating foreach() and plyr on regular as well as future.BatchJobs futures.

--- tests/foreach.R
+++ tests/foreach.R
@@ -3,7 +3,10 @@ source("incl/start.R")
 strategies <- future:::supportedStrategies()
 strategies <- setdiff(strategies, "multiprocess")
 
-message("*** doFuture() - reproducibility ...")
+## To please R CMD check
+plyr <- "plyr"
+
+message("*** doFuture - reproducibility ...")
 
 res0 <- NULL
 
@@ -25,10 +28,23 @@ for (strategy in strategies) {
     stopifnot(all.equal(res, res0))
   }
 
+  if (require(plyr, character.only=TRUE)) {
+    message("*** dplyr w / doFuture ...")
+
+    x <- list(a = 1:10, beta = exp(-3:3), logic = c(TRUE, FALSE, FALSE, TRUE))
+    y0 <- llply(x, quantile, probs = 1:3/4, .parallel = FALSE)
+    print(y0)
+    y1 <- llply(x, quantile, probs = 1:3/4, .parallel = TRUE)
+    print(y1)
+    stopifnot(all.equal(y1, y0))
+
+    message("*** dplyr w / doFuture ... DONE")
+  } ## if (require(plyr))
+
   message(sprintf("- plan('%s') ... DONE", strategy))
 } ## for (strategy ...)
 
-message("*** doFuture() - reproducibility ... DONE")
+message("*** doFuture - reproducibility ... DONE")
 
 source("incl/end.R")
 
