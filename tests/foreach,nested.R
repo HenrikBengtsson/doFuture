a620791b552d05f5d64544cfa48ccbefb77a0c7a
hb <hb@aroma-project.org>
2016-09-06 22:30:37 -0700

    TESTS: Modified nested foreach() tests

--- tests/foreach,nested.R
+++ tests/foreach,nested.R
@@ -5,15 +5,6 @@ strategies <- setdiff(strategies, "multiprocess")
 
 message("*** doFuture - nested ...")
 
-registerDoSEQ()
-
-x0 <- foreach(a=1:5, .combine='cbind') %:%
-  foreach(b=1:3, .combine='c') %do% {
-    10*a + b
-  }
-
-print(x0)
-
 registerDoFuture()
 
 for (strategy1 in strategies) {
@@ -21,12 +12,9 @@ for (strategy1 in strategies) {
     message(sprintf("- plan(list('%s', '%s')) ...", strategy1, strategy2))
     plan(list(strategy1, strategy2))
     
-    x <- foreach(a=1:5, .combine='cbind') %:%
-      foreach(b=1:3, .combine='c') %do% {
-        10*a + b
-      }
-    
-    stopifnot(identical(x, x0))
+    x <- foreach(a=1:2) %:% foreach(b=1:3) %dopar% {
+      list(a=a, b=b, plan=plan("list"))
+    } 
     
     message(sprintf("- plan(list('%s', '%s')) ... DONE", strategy1, strategy2))
   }
