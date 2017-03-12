72876b762fa83277e7c38f03821f822aa32b019f
hb <hb@aroma-project.org>
2017-03-12 14:29:17 -0700

    HELP: More informative help('doFuture')

--- incl/doFuture.R
+++ incl/doFuture.R
@@ -3,9 +3,17 @@ library("doFuture")
 registerDoFuture()
 plan(multiprocess)
 
-mu <- 1.0
-sigma <- 2.0
-foreach(i=1:3) %dopar% {
-  rnorm(i, mean=mu, sd=sigma)
+my_stat <- function(x) {
+  median(x)
 }
+
+my_experiment <- function(n, mu = 0.0, sigma = 1.0) {
+  foreach(i = 1:n) %dopar% {
+    x <- rnorm(i, mean = mu, sd = sigma)
+    list(mu = mean(x), sigma = sd(x), own = my_stat(x))
+  }
+}
+
+y <- my_experiment(n = 3)
+str(y)
 }
