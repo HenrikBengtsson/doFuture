3dd600fc4f5363c339eb6e49a1656be3845800d1
hb <hb@aroma-project.org>
2017-09-28 20:42:45 -0700

    TESTS: A tad more code coverage [ci skip]

--- tests/DEPRECATED.R
+++ tests/DEPRECATED.R
@@ -5,6 +5,8 @@ message("*** DEPRECATED & DEFUNCT ...")
 message("- options")
 
 options(doFuture.globals.nullexport = TRUE)
+y <- foreach(1:2) %dopar% TRUE
+str(y)
 res <- tryCatch({
   y <- foreach(1:2) %dopar% TRUE
 }, warning = identity)
