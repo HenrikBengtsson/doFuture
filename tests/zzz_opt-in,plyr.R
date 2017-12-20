3ff63d603000982a3b148d5ce5912bdc05f5013c
hb <hb@aroma-project.org>
2017-12-20 14:29:07 -0800

    Travis CI: Avoiding 4GB-log limit when running plyr checks [#18]

--- tests/zzz_opt-in,plyr.R
+++ tests/zzz_opt-in,plyr.R
@@ -2,6 +2,7 @@ testsets <- strsplit(Sys.getenv("_R_CHECK_TESTSETS_"), split = "[, ]")[[1]]
 print(testsets)
 if ("plyr" %in% testsets) {
   source("incl/start.R")
+  options(future.debug = FALSE) ## Otherwise too much output
   path <- system.file("tests2", package = "doFuture")
   pathname <- file.path(path, "plyr", "examples.R")
   source(pathname, echo = TRUE)
