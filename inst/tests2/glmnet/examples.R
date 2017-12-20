fc7aeb74aebc967e243bc899cd80712968f6fc8d
hb <hb@aroma-project.org>
2017-12-20 01:41:06 -0800

    TESTS: Use a dummy registerDoMC() for dont-run examples of glmnet

--- inst/tests2/glmnet/examples.R
+++ inst/tests2/glmnet/examples.R
@@ -5,12 +5,10 @@ pkg <- tests2_step("start", package = "glmnet",
                    needs = c("Suggests"))
 
 ## WORKAROUND: Some of the glmnet examples tries to use more parallel cores
-## than accepted by R CMD check, i.e. more than two.  This avoids that.
-if (require("doMC")) {
-  registerDoMC <- function(cores, ...) {
-    doMC::registerDoMC(cores = min(2L, cores))
-  }
-}
+## than accepted by R CMD check, i.e. more than two.  This causes an error
+## in itself.  Also, we don't care about testing with doMC, so we can
+## simply set up a dummy registerDoMC() here.
+registerDoMC <- function(...) NULL
 
 mprintf("*** doFuture() - all %s examples ...", pkg)
 
