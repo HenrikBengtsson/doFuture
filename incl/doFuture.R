fe9b33a0283e6f2ed9b646a99b91f024b6969d30
hb <hb@aroma-project.org>
2017-12-20 20:50:26 -0800

    Added section on Random Number Generation (RNG) to help("doFuture") and updated example("doFuture") with a best-practices RNG example [#16]

--- incl/doFuture.R
+++ incl/doFuture.R
@@ -3,17 +3,49 @@ library("doFuture")
 registerDoFuture()
 plan(multiprocess)
 
+
+## Example 1
+A <- matrix(rnorm(100^2), nrow = 100)
+B <- t(A)
+
+y1 <- apply(B, MARGIN = 2L, FUN = function(b) {
+  A %*% b
+})
+
+y2 <- foreach(b = iter(B, by='col'), .combine = cbind) %dopar% {
+  A %*% b
+}
+stopifnot(all.equal(y2, y1))
+
+
+
+## Example 2 - Simulation with parallel RNG
+library("doRNG")
+
 my_stat <- function(x) {
   median(x)
 }
 
 my_experiment <- function(n, mu = 0.0, sigma = 1.0) {
-  foreach(i = 1:n) %dopar% {
+  ## Important: use %dorng% whenever random numbers
+  ##            are involved in parallel evaluation
+  foreach(i = 1:n) %dorng% {
     x <- rnorm(i, mean = mu, sd = sigma)
     list(mu = mean(x), sigma = sd(x), own = my_stat(x))
   }
 }
 
-y <- my_experiment(n = 3)
-str(y)
+## Reproducible results when using the same RNG seed
+set.seed(0xBEEF)
+y1 <- my_experiment(n = 3)
+
+set.seed(0xBEEF)
+y2 <- my_experiment(n = 3)
+
+stopifnot(identical(y2, y1))
+
+## But only then
+y3 <- my_experiment(n = 3)
+str(y3)
+stopifnot(!identical(y3, y1))
 }
