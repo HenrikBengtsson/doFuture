b7ad7b75ab52f484d1a82cf6cf4a81ddf0ebad56
Henrik Bengtsson <hb@aroma-project.org>
2020-03-30 16:38:12 -0700

    TESTS: Increase test coverage on internal hpaste()

--- tests/utils.R
+++ tests/utils.R
@@ -1,10 +1,60 @@
 source("incl/start.R")
 
+hpaste <- doFuture:::hpaste
 stealth_sample.int <- doFuture:::stealth_sample.int
+printf <- function(...) cat(sprintf(...))
 
 message("*** utils ...")
 
-message("- stealth_sample.int()")
+message("*** hpaste() ...")
+
+# Some vectors
+x <- 1:6
+y <- 10:1
+z <- LETTERS[x]
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Abbreviation of output vector
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+printf("x = %s.\n", hpaste(x))
+## x = 1, 2, 3, ..., 6.
+
+printf("x = %s.\n", hpaste(x, maxHead = 2))
+## x = 1, 2, ..., 6.
+
+printf("x = %s.\n", hpaste(x), maxHead = 3) # Default
+## x = 1, 2, 3, ..., 6.
+
+# It will never output 1, 2, 3, 4, ..., 6
+printf("x = %s.\n", hpaste(x, maxHead = 4))
+## x = 1, 2, 3, 4, 5 and 6.
+
+# Showing the tail
+printf("x = %s.\n", hpaste(x, maxHead = 1, maxTail = 2))
+## x = 1, ..., 5, 6.
+
+# Turning off abbreviation
+printf("y = %s.\n", hpaste(y, maxHead = Inf))
+## y = 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+
+## ...or simply
+printf("y = %s.\n", paste(y, collapse = ", "))
+## y = 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+
+# Change last separator
+printf("x = %s.\n", hpaste(x, lastCollapse = " and "))
+## x = 1, 2, 3, 4, 5 and 6.
+
+# No collapse
+stopifnot(all(hpaste(x, collapse = NULL) == x))
+
+# Empty input
+stopifnot(identical(hpaste(character(0)), character(0)))
+
+message("*** hpaste() ... DONE")
+
+
+message("- stealth_sample.int() ...")
 
 if (exists(".Random.seed", envir = globalenv(), inherits = FALSE)) {
   rm(".Random.seed", envir = globalenv(), inherits = FALSE)
@@ -28,6 +78,8 @@ str(x)
 stopifnot(identical(x, x0))
 seed <- globalenv()[[".Random.seed"]]
 stopifnot(!is.null(seed), identical(seed, seed0))
+
+message("- stealth_sample.int() ... DONE")
 
 message("*** utils ... DONE")
 
