d411c33cb200249973c87bb5958a90cb883623c8
Henrik Bengtsson <hb@aroma-project.org>
2020-03-30 16:34:56 -0700

    CLEANUP: Remove import_from() and import_future(); no longer needed

--- R/utils.R
+++ R/utils.R
@@ -88,21 +88,6 @@ stealth_sample.int <- function(n, size = n, replace = FALSE, ...) {
 }
 
 
-import_from <- function(name, default = NULL, package) {
-  ns <- getNamespace(package)
-  if (exists(name, mode = "function", envir = ns, inherits = FALSE)) {
-    get(name, mode = "function", envir = ns, inherits = FALSE)
-  } else if (!is.null(default)) {
-    default
-  } else {
-    stop(sprintf("No such '%s' function: %s()", package, name))
-  }
-}
-
-import_future <- function(name, default = NULL) {
-  import_from(name, default = default, package = "future")
-}
-
 #' @importFrom utils packageVersion
 future_version <- local({
   ver <- NULL
