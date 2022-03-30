6ed4610d5bf4075f54f2407567cfcbedbbbfc5ba
Henrik Bengtsson <hb@aroma-project.org>
2022-03-30 11:43:13 -0700

    TESTS: Third attempts to shut down nested workers to avoid 'checking for detritus in the temp directory ... NOTE'

--- tests/foreach,nested_dopar.R
+++ tests/foreach,nested_dopar.R
@@ -105,10 +105,29 @@ for (strategy1 in strategies) {
     ## from 'R CMD check --as-cran' when running on MS Windows.
     message("- shut down nested workers")
     dummy <- foreach(a = as) %dopar% {
+      ## AD HOC: Identify any PSOCK cluster and close it
+      p <- future::plan("next")
+      if (inherits(p, "cluster")) {
+        f <- future::future(NULL)
+        v <- future::value(f)
+        if (inherits(f$workers, "cluster")) parallel::stopCluster(f$workers)
+        f <- NULL
+      }
       future::plan("sequential")
       gc()
     }
 
+    ## AD HOC: Identify any PSOCK cluster and close it
+    p <- future::plan("next")
+    if (inherits(p, "cluster")) {
+      f <- future::future(NULL)
+      v <- future::value(f)
+      if (inherits(f$workers, "cluster")) parallel::stopCluster(f$workers)
+      f <- NULL
+    }
+    future::plan("sequential")
+    gc()
+    
     ## Cleanup in order make sure none of these variables exist as
     ## proxies for missing globals of the name names
     rm(list = c("as", "bs", "x"))
