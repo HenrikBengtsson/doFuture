41c858c0c373cc28bb4730b62a1603290b9db9b6
Henrik Bengtsson <hb@aroma-project.org>
2022-03-30 14:05:32 -0700

    thinko

--- tests/foreach,nested.R
+++ tests/foreach,nested.R
@@ -11,7 +11,8 @@ for (strategy1 in strategies) {
   for (strategy2 in strategies) {
     message(sprintf("- plan(list('%s', '%s')) ...", strategy1, strategy2))
     plan(list(a = strategy1, b = strategy2))
-
+    nested <- plan("list")
+    
     as <- 1:2
     bs <- 3:1
     
@@ -41,7 +42,7 @@ for (strategy1 in strategies) {
 
     ## Cleanup in order make sure none of these variables exist as
     ## proxies for missing globals of the name names
-    rm(list = c("as", "bs", "x"))
+    rm(list = c("as", "bs", "x", "nested"))
 
     ## WORKAROUND: Manually shut down *nested* parallel workers to avoid
     ## * checking for detritus in the temp directory ... NOTE
