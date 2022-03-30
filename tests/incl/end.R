dd9141ece17f21ba9c45e5bf8da2697fc8e8516a
Henrik Bengtsson <hb@aroma-project.org>
2022-03-30 16:57:10 -0700

    TESTS: Report on stray connections after finishing tests

--- tests/incl/end.R
+++ tests/incl/end.R
@@ -10,6 +10,11 @@ rm(list = c(setdiff(ls(), ovars)))
 cons1 <- showConnections(all = FALSE)
 diff <- all.equal(cons1, cons0)
 if (!isTRUE(diff)) {
+  cat("Connections before:\n")
+  print(cons0)
+  cat("Connections after:\n")
+  print(cons1)
+  cat("Difference:\n")
   print(diff)
   stop("[INTERNAL ERROR] Detected stray connections after finishing test")
 }
