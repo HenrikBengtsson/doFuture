4e57f1ea5f03b3ae9ffc265e1c6b38b3df999abe
Henrik Bengtsson <hb@aroma-project.org>
2022-03-30 17:06:09 -0700

    TESTS: Record 'before' connections after loading the future package

--- tests/incl/start,load-only.R
+++ tests/incl/start,load-only.R
@@ -1,11 +1,14 @@
 ## Record original state
-cons0 <- showConnections(all = FALSE)
 ovars <- ls()
 oopts <- options(warn = 1L,
                  mc.cores = 2L,
                  future.debug = TRUE,
                  doFuture.debug = TRUE)
 oplan <- future::plan()
+
+## Record connected *after* the future package has been loaded,
+## because the default plan might be a PSOCK cluster
+cons0 <- showConnections(all = FALSE)
 
 future::plan(future::sequential)
 doFuture::registerDoFuture()
