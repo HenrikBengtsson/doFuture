cdbb6962a2c5c43a426b2ef7495000a6c156681a
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2023-03-08 16:59:01 +0100

    HELP: Add more details on %dofuture%

--- R/dofuture_OP.R
+++ R/dofuture_OP.R
@@ -19,6 +19,116 @@
 #'    whenever using random numbers in the `expr` expression)
 #' * global variables and packages are identified automatically by
 #'   the \pkg{future} framework
+#' * errors are relayed as-is (with `%dopar%` they captured and modified)
+#'
+#'
+#' @section Global variables and packages:
+#' When using `%dofuture%`, the future framework identifies globals and
+#' packages automatically (via static code inspection).  However, there
+#' are cases where it fails to find some of the globals or packages. When
+#' this happens, one can specify the [future::future()] arguments `globals`
+#' and `packages` via foreach argument `.options.future`.  For example,
+#' if you specify argument
+#' `.options.future = list(globals = structure(TRUE, ignore = "b", add = "a"))`
+#' then globals are automatically identified (`TRUE`), but it ignores `b` and
+#' always adds `a`.
+#' For further details and instructions, see [future::future()].
+#'
+#'
+#' @section Random Number Generation (RNG):
+#' The `%dofuture%` uses the future ecosystem to generate proper random
+#' numbers in parallel in the same way they are generated in, for instance,
+#' \pkg{future.apply}. For this to work, you need to specify
+#' `.options.future = list(seed = TRUE)`.  For example,
+#'
+#' ```r
+#' y <- foreach(i = 1:3, .options.future = list(seed = TRUE)) %dofuture% {
+#'   rnorm(1)
+#' }
+#' ```
+#'
+#' Unless `seed` is `FALSE` or `NULL`, this guarantees that the exact same
+#' sequence of random numbers are generated _given the same initial
+#' seed / RNG state_ - this regardless of type of future backend, number of
+#' workers, and scheduling ("chunking") strategy.
+#' 
+#' RNG reproducibility is achieved by pregenerating the random seeds for all
+#' iterations by using L'Ecuyer-CMRG RNG streams.  In each
+#' iteration, these seeds are set before evaluating the foreach expression.
+#' _Note, for large number of iterations this may introduce a large overhead._
+#'
+#' If `seed = TRUE`, then \code{\link[base:Random]{.Random.seed}}
+#' is used if it holds a L'Ecuyer-CMRG RNG seed, otherwise one is created
+#' randomly.
+#'
+#' If `seed = FALSE`, it is expected that none of the foreach iterations
+#' use random number generation.
+#' If they do, then an informative warning or error is produces depending
+#' on settings. See [future::future] for more details.
+#' Using `seed = NULL`, is like `seed = FALSE` but without the check
+#' whether random numbers were generated or not.
+#'
+#' As input, `seed` may also take a fixed initial seed (integer),
+#' either as a full L'Ecuyer-CMRG RNG seed (vector of 1+6 integers), or
+#' as a seed generating such a full L'Ecuyer-CMRG seed. This seed will
+#' be used to generated one L'Ecuyer-CMRG RNG stream for each iteration.
+#'
+#' For further details and instructions, see
+#' [future.apply::future_lapply()].
+#'
+#'
+#' @section Load balancing ("chunking"):
+#' Whether load balancing ("chunking") should take place or not can be
+#' controlled by specifying either argument
+#' `.options.future = list(scheduling = <ratio>)` or
+#' `.options.future = list(chunk.size = <count>)` to `foreach()`.
+#'
+#' The value `chunk.size` specifies the average number of elements
+#' processed per future ("chunks").
+#' If `+Inf`, then all elements are processed in a single future (one worker).
+#' If `NULL`, then argument `future.scheduling` is used.
+#'
+#' The value `scheduling` specifies the average number of futures
+#' ("chunks") that each worker processes.
+#' If `0.0`, then a single future is used to process all iterations;
+#' none of the other workers are not used.
+#' If `1.0` or `TRUE`, then one future per worker is used.
+#' If `2.0`, then each worker will process two futures (if there are
+#' enough iterations).
+#' If `+Inf` or `FALSE`, then one future per iteration is used.
+#' The default value is `scheduling = 1.0`.
+#'
+#' For further details and instructions, see
+#' [future.apply::future_lapply()].
+#'
+#'
+#' @section Control processing order of iterations:
+#' Attribute `ordering` of `chunk.size` or `scheduling` can be used to
+#' control the ordering the elements are iterated over, which only affects
+#' the processing order and _not_ the order values are returned.
+#' This attribute can take the following values:
+#'
+#' * index vector - an numeric vector of length `nX`.
+#'
+#' * function     - an function taking one argument which is called as
+#'                  `ordering(nX)` and which must return an
+#'                  index vector of length `nX`, e.g.
+#'                  `function(n) rev(seq_len(n))` for reverse ordering.
+#'
+#' * `"random"`   - this will randomize the ordering via random index
+#'                  vector `sample.int(nX)`.
+#'
+#' where `nX` is the number of foreach iterations to be done.
+#'
+#' For example,
+#' `.options.future = list(scheduling = structure(2.0, ordering = "random"))`.
+#'
+#' _Note_, when elements are processed out of order, then captured standard
+#' output and conditions are also relayed in that order, that is, out of order.
+#'
+#' For further details and instructions, see
+#' [future.apply::future_lapply()].
+#'
 #'
 #' @example incl/dofuture_OP.R
 #'
