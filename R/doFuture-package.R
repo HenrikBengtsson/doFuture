d253bb94b36e2b156b6b523dc9dacea366689a0e
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2023-03-08 22:27:42 +0100

    HELP: List both alternatives in ?doFuture

--- R/doFuture-package.R
+++ R/doFuture-package.R
@@ -4,7 +4,20 @@
 #' \pkg{foreach} package together with the \pkg{future} package
 #' such that `foreach()` parallelizes via _any_ future backend.
 #'
-#' For example,
+#' @section Usage:
+#' There are two alternative ways to use this package:
+#'
+#' 1. `y <- foreach(...) %dopar% { ... }` with `registerDoFuture()`
+#' 2. `y <- foreach(...) %dofuture% { ... }`
+#'
+#' The _first alternative_ is based on the traditional **foreach**
+#' approach where one registers a foreach adapter to be used by `%dopar%`.
+#' A popular adapter is `doParallel::registerDoParallel()`, which
+#' parallelizes on the local machine using the **parallel** package.
+#' This package provides `registerDoFuture()`, which parallelizes using
+#' the **future** package, meaning any future-compliant parallel backend
+#' can be used.
+#' An example is:
 #'
 #' ```r
 #' library(doFuture)
@@ -17,9 +30,28 @@
 #' }
 #' ```
 #'
-#' will parallelize the foreach iterations on the local machine.
+#' See [registerDoFuture()] for more details and examples on this approach.
 #'
-#' For more details and examples, see [registerDoFuture()].
+#' The _second alternative_, which uses `%dofuture%`, avoids having to use
+#' `registerDoFuture()`.  The `%dofuture%` operator provides a more
+#' consistent behavior than `%dopar%`, e.g. there is a unique set of
+#' foreach arguments instead of one per possible adapter.  Identification
+#' of globals, random number generation (RNG), and error handling is
+#' handled by the future ecosystem, just like with other map-reduce
+#' solutions such as **future.apply** and **furrr**.
+#' An example is:
+#'
+#' ```r
+#' library(doFuture)
+#' plan(multisession)
+#'
+#' y <- foreach(x = 1:4, y = 1:10) %dofuture% {
+#'   z <- x + y
+#'   slow_sqrt(z)
+#' }
+#' ```
+#'
+#' See [`%dofuture%`] for more details and examples on this approach.
 #'
 #' @docType package
 #' @aliases doFuture-package
