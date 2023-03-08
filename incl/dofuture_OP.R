7d6d4d0db35bc714f4eddd08412d145a6579b937
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2023-03-08 10:46:06 +0100

    Make sure %dofuture% example shuts down any parallel workers

--- incl/dofuture_OP.R
+++ incl/dofuture_OP.R
@@ -1,5 +1,14 @@
+\donttest{
+plan(multisession)  # parallelize futures on the local machine
+
 y <- foreach(x = 1:10, .combine = rbind) %dofuture% {
   y <- sqrt(x)
   data.frame(x = x, y = y, pid = Sys.getpid())
 }
 print(y)
+}
+
+\dontshow{
+## R CMD check: make sure any open connections are closed afterward
+if (!inherits(plan(), "sequential")) plan(sequential)
+}
