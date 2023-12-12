4d5ea93a1bb40712a2e6cbb5958b15c71a5e661a
BHGC Website GHA Workflow Runner <bhgc-github-actions-publish@bhgc.org>
2023-12-11 17:54:25 -0800

    DOCS: Recommend %dofuture% over %dopar% (fix #78)

--- R/doFuture-package.R
+++ R/doFuture-package.R
@@ -7,10 +7,35 @@
 #' @section Usage:
 #' There are two alternative ways to use this package:
 #'
-#' 1. `y <- foreach(...) %dopar% { ... }` with `registerDoFuture()`
-#' 2. `y <- foreach(...) %dofuture% { ... }`
+#' 1. `y <- foreach(...) %dofuture% { ... }`
+#' 2. `y <- foreach(...) %dopar% { ... }` with `registerDoFuture()`
 #'
-#' The _first alternative_ is based on the traditional **foreach**
+#' The _first alternative_ (recommended), which uses [`%dofuture%`], avoids
+#' having to use `registerDoFuture()`.  The [`%dofuture%`] operator provides
+#' a more consistent behavior than `%dopar%`, e.g. there is a unique set of
+#' foreach arguments instead of one per possible adapter.  Identification
+#' of globals, random number generation (RNG), and error handling is
+#' handled by the future ecosystem, just like with other map-reduce
+#' solutions such as **[future.apply]** and **[furrr]**.
+#' An example is:
+#'
+#' ```r
+#' library(doFuture)
+#' plan(multisession)
+#'
+#' y <- foreach(x = 1:4, y = 1:10) %dofuture% {
+#'   z <- x + y
+#'   slow_sqrt(z)
+#' }
+#' ```
+#'
+#' This alternative is the recommended way to let `foreach()` parallelize
+#' via the future framework if you start out from scratch.
+#'
+#' See [`%dofuture%`] for more details and examples on this approach.
+#'
+#'
+#' The _second alternative_ is based on the traditional **foreach**
 #' approach where one registers a foreach adapter to be used by `%dopar%`.
 #' A popular adapter is `doParallel::registerDoParallel()`, which
 #' parallelizes on the local machine using the **parallel** package.
@@ -42,29 +67,6 @@
 #'
 #' See [registerDoFuture()] for more details and examples on this approach.
 #'
-#' The _second alternative_, which uses [`%dofuture%`], avoids having to use
-#' `registerDoFuture()`.  The [`%dofuture%`] operator provides a more
-#' consistent behavior than `%dopar%`, e.g. there is a unique set of
-#' foreach arguments instead of one per possible adapter.  Identification
-#' of globals, random number generation (RNG), and error handling is
-#' handled by the future ecosystem, just like with other map-reduce
-#' solutions such as **[future.apply]** and **[furrr]**.
-#' An example is:
-#'
-#' ```r
-#' library(doFuture)
-#' plan(multisession)
-#'
-#' y <- foreach(x = 1:4, y = 1:10) %dofuture% {
-#'   z <- x + y
-#'   slow_sqrt(z)
-#' }
-#' ```
-#'
-#' This alternative is the recommended way to let `foreach()` parallelize
-#' via the future framework if you start out from scratch.
-#'
-#' See [`%dofuture%`] for more details and examples on this approach.
 #'
 #' [future.apply]: https://cran.r-project.org/package=future.apply
 #' [furrr]: https://cran.r-project.org/package=furrr
